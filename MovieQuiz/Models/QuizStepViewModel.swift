2a2ac17eabfcdbea19465c3d0c2596fcf42aaf5c
valentuk <4952214@gmail.com>
2025-09-19 14:52:22 +0300

    sprint_05

---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by  Павел Валентюк on 10.09.2025.
+//
+
+import Foundation
+import UIKit
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,37 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by  Павел Валентюк on 15.09.2025.
+//
+
+import UIKit
+
+struct AlertPresenter {
+    private let alertModel: AlertModel
+    
+    weak var viewController: UIViewController?
+    
+    func requestAlert() {
+        
+        let alert = UIAlertController(
+            title: alertModel.title,
+            message: alertModel.message,
+            preferredStyle: .alert)
+    
+        let action = UIAlertAction(
+            title: alertModel.buttonText,
+            style: .default,
+            handler: {_ in
+                alertModel.completion()
+            })
+    
+        alert.addAction(action)
+    
+        viewController?.present(alert, animated: true, completion: nil)
+    }
+    
+    init(alertModel: AlertModel) {
+        self.alertModel = alertModel
+    }
+}
